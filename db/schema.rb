e542a767d8219429bde5014bdd33923dcacb62f6
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2014-06-08 15:25:17 +0200

    Missing migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140608125911) do
+ActiveRecord::Schema.define(version: 20140608131019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,10 @@ ActiveRecord::Schema.define(version: 20140608125911) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "type"
+    t.string   "email",              default: "", null: false
+    t.string   "encrypted_password", default: "", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
