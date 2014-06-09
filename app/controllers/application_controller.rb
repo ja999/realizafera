0ed122009ae49a898009d02d72ba4fa2cc1b959d
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2014-06-09 10:46:19 +0200

    Pass environment and user to ton

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   alias_method :h, :view_context
 
+  before_filter :pass_variables_to_gon
+
+  private
+
+  def pass_variables_to_gon
+    gon.environment = Rails.env
+    gon.current_user = UserSerializer.new(current_user)
+  end
 end
