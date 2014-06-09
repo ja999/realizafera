1d67919c2f3cc327d9fb220bec57fae96da199d7
Aleksander Wdowiński <aleksander.wdowinski@netguru.pl>
2014-06-09 11:15:54 +0200

    add params for productions

--- app/controllers/productions_controller.rb
+++ app/controllers/productions_controller.rb
@@ -1,6 +1,14 @@
 class ProductionsController < ApplicationController
-  expose :production
+  expose(:production, attributes: :production_params)
   respond_to :json
 
-  def create; end
+  def create
+
+  end
+
+  private
+
+  def production_params
+    params.require(:production).slice(:start_day, :start_hour, :end_day, :end_hour, :repetitive)
+  end
 end
