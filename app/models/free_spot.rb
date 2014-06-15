7c92bce54b83179b2d92be799b75d5034715b478
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2014-06-15 17:54:47 +0200

    validations for events

--- app/models/free_spot.rb
+++ app/models/free_spot.rb
@@ -1,2 +1,5 @@
 class FreeSpot < ActiveRecord::Base
+  validate :start_day, :end_day,       inclusion: { in: (0..6)  }
+  validate :start_hour, :end_hour,     inclusion: { in: (0..23) }
+  validate :start_minute, :end_minute, inclusion: { in: (0..59) }
 end
