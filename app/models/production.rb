c1c37b21a741761db5f8cba95ddc5491dd8d93b5
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2014-06-15 17:56:17 +0200

    Production #incident_with_wday

--- app/models/production.rb
+++ app/models/production.rb
@@ -2,4 +2,11 @@ class Production < ActiveRecord::Base
   validate :start_day, :end_day,       inclusion: { in: (0..6)  }
   validate :start_hour, :end_hour,     inclusion: { in: (0..23) }
   validate :start_minute, :end_minute, inclusion: { in: (0..59) }
+
+  def self.incident_with_wday(wday)
+    where("
+       (start_day <= end_day AND start_day <= ? AND ? <= end_day)
+    OR (start_day > end_day AND (start_day <= ? OR ? <= end_day))
+    ", wday, wday, wday, wday)
+  end
 end
