9577def3742862d49d8f8edc9181fc6236643c41
Aleksander Wdowiński <aleksander.wdowinski@netguru.pl>
2014-06-15 20:58:34 +0200

    Add edit view for FreeSpot

--- app/models/free_spot.rb
+++ app/models/free_spot.rb
@@ -3,4 +3,13 @@ class FreeSpot < ActiveRecord::Base
   validate :start_day, :end_day,       inclusion: { in: (0..6)  }
   validate :start_hour, :end_hour,     inclusion: { in: (0..23) }
   validate :start_minute, :end_minute, inclusion: { in: (0..59) }
+  DAYS = {
+    'poniedziałek' => '0',
+    'wtorek' => '1',
+    'środa' => '2',
+    'czwartek' => '3',
+    'piątek' => '4',
+    'sobota' => '5',
+    'niedziela' => '6',
+  }
 end
