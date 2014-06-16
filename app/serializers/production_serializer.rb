96e30598037b42fff6882a6f36f8ad9db47bb422
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2014-06-16 10:55:21 +0200

    Send more attributes

--- app/serializers/production_serializer.rb
+++ app/serializers/production_serializer.rb
@@ -1,5 +1,5 @@
 class ProductionSerializer < ActiveModel::Serializer
-  attributes :id, :errors, :user_id, :start_day, :start_hour, :end_day, :end_hour
+  attributes :id, :errors, :user_id, :start_day, :start_hour, :start_minute, :end_day, :end_hour, :end_minute
   attributes :repetitive, :cancelled, :confirmed_by_user
 end
 
