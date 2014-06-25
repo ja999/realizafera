e1d141b188a62f9efa072c564d38dd3fca5f9691
Aleksander Wdowiński <aleksander.wdowinski@netguru.pl>
2014-06-25 16:58:39 +0200

    Add validations to users, change production edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable
 
+  validates :email, :first_name, :last_name, presence: true
+
   has_many :free_spots
   has_many :productions
 end
