7b1dec8371781c1b1628f7695786a039831475cb
Artur Warejko <artur.warejko@student.put.poznan.pl>
2014-06-27 01:33:06 +0200

    incoming_productions_reminder should work.

--- app/mailers/production_mailer.rb
+++ app/mailers/production_mailer.rb
@@ -17,29 +17,35 @@ class ProductionMailer < ActionMailer::Base
   end
 
 
-  def remind_about_productions (productions)
+  def remind_about_productions (production)
     # TODO
   end
 
 
   def incoming_productions_reminder
     # This method is regularly called by cron
+
     Production.all.each do |p|
       # For each comming production
       dayDiff = (p.start_day + 7 - Time.now.wday) % 7
       hourDiff = 24 * dayDiff + p.start_hour - Time.now.hour
 
       # If production is close (at most 30h) and wasn't reminded yet then remind about it
-      if hourDiff < 30 # TODO && p.reminded == false
+      if hourDiff < 30 && p.reminded == false
         msg = ProductionMailer.remind_about_productions(p)
         msg.deliver
+
+        p.reminded = true
+        p.save
       end
     end
   end
 
   def weekly_reminder
     # This method is regularly called by cron
-    # TODO
+    Production.all.each do |p|
+      # TODO
+    end
   end
 
 end
