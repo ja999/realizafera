91695d41b3f930e9171ecb1b82f6715e28464868
Aleksander Wdowiński <aleksander.wdowinski@netguru.pl>
2014-06-27 17:20:43 +0200

    Add authenticate users, styling and stuff

--- app/controllers/graphics_controller.rb
+++ app/controllers/graphics_controller.rb
@@ -2,6 +2,8 @@ class GraphicsController < ApplicationController
   respond_to :json
   expose(:graphic) { Graphic.new(permitted_attributes) }
 
+  before_filter :authenticate_admin_user!
+
   def show
     render json: graphic, serializer: GraphicSerializer
   end
